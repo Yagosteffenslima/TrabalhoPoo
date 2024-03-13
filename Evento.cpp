37bcbdd2c43746e88b861e463e650504db121c02
Yagosteffenslima <162059565+Yagosteffenslima@users.noreply.github.com>
2024-03-13 13:52:53 -0300

    Delete Evento.cpp

--- Evento.cpp
+++
@@ -1,9 +0,0 @@
-#include "Evento.h"
-
-// Implementação do construtor da classe Evento
-Evento::Evento(int timeStamp) : timeStamp(timeStamp) {}
-
-// Implementação do método getTimeStamp da classe Evento
-int Evento::getTimeStamp() const {
-    return timeStamp;
-}
